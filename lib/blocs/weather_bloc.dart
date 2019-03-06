d3d2acc6e03ae55b7c1d4003c4e8c008dac09a19
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-03-06 23:07:30 +0200

    Initial implementation of weather bloc

---
+++ lib/blocs/weather_bloc.dart
@@ -0,0 +1,34 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+import 'package:aurinko/models/models.dart';
+
+abstract class WeatherEvent extends Equatable {
+  WeatherEvent([List props = const []]) : super(props);
+}
+
+class FetchWeather extends WeatherEvent {
+  final String city;
+
+  FetchWeather({@required this.city})
+      : assert(city != null),
+        super([city]);
+}
+
+abstract class WeatherState extends Equatable {
+  WeatherState([List props = const []]) : super(props);
+}
+
+class WeatherEmpty extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherLoaded extends WeatherState {
+  final Weather weather;
+
+  WeatherLoaded({@required this.weather})
+      : assert(weather != null),
+        super([weather]);
+}
+
+class WeatherError extends WeatherState {}
