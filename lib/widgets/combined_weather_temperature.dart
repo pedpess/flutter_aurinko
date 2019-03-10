f037f49fc8c6f357e74a907d4b81d21d10dbb4c1
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-03-10 12:20:43 +0200

    Initial widgets to build the weather

---
+++ lib/widgets/combined_weather_temperature.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:meta/meta.dart';
+
+import 'package:aurinko/models/models.dart' as model;
+import 'package:aurinko/widgets/widgets.dart';
+
+class CombinedWeatherTemperature extends StatelessWidget {
+  final model.Weather weather;
+
+  CombinedWeatherTemperature({Key key, @required this.weather})
+      : assert(weather != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: WeatherConditions(condition: weather.condition),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Temperature(
+                temperature: weather.temp,
+                high: weather.maxTemp,
+                low: weather.minTemp,
+              ),
+            )
+          ],
+        ),
+        Center(
+          child: Text(
+            weather.formattedCondition,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w200,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,7 @@
+export 'weather.dart';
+export 'location.dart';
+export 'last_update.dart';
+export 'combined_weather_temperature.dart';
+export 'temperature.dart';
+export 'weather_conditions.dart';
+export 'city_selection.dart';
