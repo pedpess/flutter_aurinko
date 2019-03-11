6ec8dba5caecef06f438d8b213b2bbd7bafaf1ab
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-03-11 21:52:44 +0200

    Introduce theme bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:bloc/bloc.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:aurinko/widgets/widgets.dart';
+import 'package:aurinko/blocs/blocs.dart';
 import 'package:aurinko/repositories/repositories.dart';
 import 'package:aurinko/api/api.dart';
 
@@ -25,7 +27,7 @@ void main() {
   runApp(App(weatherRepository: weatherRepository));
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   final WeatherRepository weatherRepository;
 
   App({Key key, @required this.weatherRepository})
@@ -33,12 +35,34 @@ class App extends StatelessWidget {
         super(key: key);
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  ThemeBloc _themeBloc = ThemeBloc();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Aurinko',
-      home: Weather(
-        weatherRepository: weatherRepository,
+    return BlocProvider(
+      bloc: _themeBloc,
+      child: BlocBuilder(
+        bloc: _themeBloc,
+        builder: (_, ThemeState themeState) {
+          return MaterialApp(
+            title: 'Aurinko',
+            theme: themeState.theme,
+            home: Weather(
+              weatherRepository: widget.weatherRepository,
+            ),
+          );
+        },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _themeBloc.dispose();
+    super.dispose();
   }
 }
